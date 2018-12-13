b65b2fa9ca0d7d8a709756d30ce742014734fb15
vishalf15 <vishalf15@gmail.com>
2018-12-13 21:41:53 +0530

    jar file location changed
    jar file location changed

--- start-addition-service.sh
+++ start-addition-service.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd '/home/ec2-user/apps/addition-service'
+cd '/home/ec2-user/apps/hello-word'
 echo 'EC2 list before copying content from S3 to EC2 = '
 ls
 
@@ -10,8 +10,8 @@ echo 'Starting copying jar from S3 bucket to EC2'
 aws deploy push --application-name hello-word-jar-type --s3-location s3://pract-code-deploy/hello-word-jar-type-1.0.0.jar --ignore-hidden-files
 echo 'Jar copy completed from S3 bucket to EC2'
 
-cd '/home/ec2-user/apps/addition-service'
-echo 'EC2 list = '
+echo 'EC2 list After copying content from S3 to EC2 = '
 ls
+
 echo 'Starting Spring Boot app'
 nohup java -jar hello-word-jar-type-1.0.0.jar > /dev/null &
