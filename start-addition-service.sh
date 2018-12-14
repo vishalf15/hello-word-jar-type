0ed65765237fd13dbe984bd3d3794c0841fe0d34
vishalf15 <vishalf15@gmail.com>
2018-12-14 21:26:02 +0530

    code build artifact locatioon changed

--- start-addition-service.sh
+++ start-addition-service.sh
@@ -4,7 +4,7 @@ echo 'EC2 list before copying content from S3 to EC2 = '
 ls
 
 echo 'S3 list = '
-aws s3 ls s3://pract-code-build/hello-word/target/
+aws s3 ls s3://pract-code-build/hello-word-jar-type/target
 
 echo 'Starting copying jar from S3 bucket to EC2'
 aws s3 cp s3://pract-code-build/hello-word-jar-type/target/hello-word-jar-type-1.0.0.jar hello-word-jar-type-1.0.0.jar
