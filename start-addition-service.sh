0231d336582b7fce614921dea606d0bdc11b098d
vishalf15 <vishalf15@gmail.com>
2018-12-14 21:16:18 +0530

    change in s3 bucket location
    change in s3 bucket location

--- start-addition-service.sh
+++ start-addition-service.sh
@@ -7,7 +7,7 @@ echo 'S3 list = '
 aws s3 ls s3://pract-code-build/hello-word/target/
 
 echo 'Starting copying jar from S3 bucket to EC2'
-aws s3 cp s3://pract-code-build/hello-word/target/hello-word-jar-type-1.0.0.jar hello-word-jar-type-1.0.0.jar
+aws s3 cp s3://pract-code-build/hello-word-jar-type/target/hello-word-jar-type-1.0.0.jar hello-word-jar-type-1.0.0.jar
 echo 'Jar copy completed from S3 bucket to EC2'
 
 echo 'EC2 list After copying content from S3 to EC2 = '
